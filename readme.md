cd1a58f9edbb4fc93537b897fc31d887a377a66c
saiess <saidessalami0@gmail.com>
2022-06-22 00:22:09 +0100

    updates

---
+++ readme.md
@@ -0,0 +1,27 @@
+# Webpack App Landing Page
+
+Includes the following features...
+
+- Babel Loader
+- Sass Loader
+- HTMLWebpackPlugin
+- BundleAnalyzerPlugin
+- WebpackDevServer
+- Asset Resource Loader
+- Source Maps
+- Caching
+- Bootstrap 5.2
+- postcss-loader
+
+## Usage
+
+(Port 3000)
+```
+  yarn dev
+```
+
+## Build for production
+
+```
+yarn build
+```
--- webpack.config.js
+++ webpack.config.js
@@ -21,8 +21,8 @@ module.exports = {
     },
     port: 3000,
     open: true,
-    compress: true,
     hot: true,
+    compress: true,
     historyApiFallback: true,
   },
   module: {
@@ -42,7 +42,11 @@ module.exports = {
         },
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(svg|png|jpg|jpeg|gif|webp)$/i,
         type: 'asset/resource',
       },
     ],
